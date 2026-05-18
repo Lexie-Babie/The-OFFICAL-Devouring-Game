02a93014a626897cd0ff60ca864b3fbd30be2030
Rebecca Klass <klassr2019@gmail.com>
2026-05-18 10:20:29 +0200

    Pushing all changes
    Pushing the addition of all scenes and the basics of everything

---
+++ The_OFFICIAL_DevouringGamePj/Assets/Scripts/Unit.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int hpSlider;
+
+    public string unitName;
+
+    public int damage;
+    public int heal;
+    public int cook;
+
+    public int maxHP;
+    public int currentHP;
+
+    public bool TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHP += amount;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
+
+
+    public bool CookDamage(int cook)
+    {
+        currentHP -= cook;
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+
+}
