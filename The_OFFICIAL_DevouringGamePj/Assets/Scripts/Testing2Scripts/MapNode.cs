37e8382f68329f00b2ae13263e8b8cbcd47f4914
Lexie-Babie <alexandra.peters001@gmail.com>
2026-05-20 10:04:26 +0200

    Map Movement and node connection
    player piece moves to nodes, and unlocking and locking nodes works. just needs to add actual functionality to nodes

---
+++ The_OFFICIAL_DevouringGamePj/Assets/Scripts/Testing2Scripts/MapNode.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MapNode : MonoBehaviour //holds Node info and handles visuals
+{
+    public List<MapNode> connectedNodes = new List<MapNode>();
+    public bool isVisited = false;
+    public bool isReachable = false;
+    public Color reachableColor = Color.white;
+    public Color unreachableColor = new Color(0.4f, 0.4f, 0.4f, 1f);
+    private SpriteRenderer spriteRenderer;
+
+    void Start() //accesess the SpriteRenderer component and updates the visual representation of the node based on its reachability status
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateVisual();
+    }
+
+    public void SetReachable(bool value) //sets the reachability status of the node and updates its visual representation accordingly
+    {
+        isReachable = value;
+        UpdateVisual();
+    }
+
+    void UpdateVisual() //handles the colour change stuff indicating if reachalbe or not 
+    {
+        if (spriteRenderer == null) return;
+        if (isReachable)
+        {
+            spriteRenderer.color = reachableColor;
+
+        }
+        else
+        {
+            spriteRenderer.color = unreachableColor;
+
+        }
+    }
+}
