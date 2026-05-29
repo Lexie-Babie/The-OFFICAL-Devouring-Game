2ddceab4899e5c37835791efab08c5d7e4d8957a
Lexie-Babie <alexandra.peters001@gmail.com>
2026-05-30 01:09:23 +0200

    refining the map scene and connecting up the scenes
    added new map background, added dynamic lines to node, added relavent scenes to scene transition nodes (only tutscene and tutcombat scene though), added in sprites to map ui

--- The_OFFICIAL_DevouringGamePj/Assets/Scripts/MapSceneScripts/MapNode.cs
+++ The_OFFICIAL_DevouringGamePj/Assets/Scripts/MapSceneScripts/MapNode.cs
@@ -22,6 +22,22 @@ public class MapNode : MonoBehaviour //holds Node info and handles visuals
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         UpdateVisual();
+
+        foreach (MapNode connected in connectedNodes) // adds the lines to the nodes more dynamically (do i don't have to redraw them every time if i move them)
+        {
+            GameObject lineObj = new GameObject("Line_" + gameObject.name + "_to_" + connected.gameObject.name);
+            LineRenderer lr = lineObj.AddComponent<LineRenderer>();
+
+            lr.material = new Material(Shader.Find("Sprites/Default"));
+            lr.positionCount = 2;
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, connected.transform.position);
+            lr.startWidth = 0.1f;
+            lr.endWidth = 0.1f;
+            lr.startColor = Color.red; 
+            lr.endColor = Color.red;
+            lr.sortingOrder = -1;
+        }
     }
 
     public void SetReachable(bool value) //sets the reachability status of the node and updates its visual representation accordingly
